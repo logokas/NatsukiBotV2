49a7fcd6c02a6b6669bd95bc5ea868ef30aa51ed
GanstaKingofSA <azarieldc@gmail.com>
2020-09-15 17:38:58 -0500

    [owner/blacklist] dyno feats and bl update

--- cogs/blacklist.py
+++ cogs/blacklist.py
@@ -1,9 +1,10 @@
 ## Blacklist.py Guide
-# To add new mods/sites/words to the ban list, enter the respective TXT file and type the new word to censor.
+# To add new mods/words/whitelist additional mod names to the ban list, enter the respective TXT file and type the new word to censor.
 # Save and the bot should already see the new term.
 # Example:
 # exit music
 # comedy club
+# a brand new day
 # abridged
 
 import discord
@@ -15,6 +16,10 @@ class BlacklistCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+        '''
+        Manages the Censor Mode of Natsu with New Accounts
+        and Site/Mod Bans
+        '''
         # checks if message is not from NatBot
         if message.author.id == 626045764149444614:
             return
@@ -43,7 +48,7 @@ class BlacklistCog(commands.Cog):
 
             # checks if message contains any words in the txt file
             for x in range(sblLength):
-                sblRegex = r"("+siteBlacklist[x]+"\.)"
+                sblRegex = r"("+siteBlacklist[x]+")"
                 sblMatches = re.finditer(sblRegex, message.content.lower(), re.MULTILINE | re.IGNORECASE)
                 for x in enumerate(sblMatches, start=1):
                     # checks if user is not a mod
@@ -62,17 +67,23 @@ class BlacklistCog(commands.Cog):
                 wordList = [wordNames.strip().lower() for wordNames in wordFile.readlines()]
                 wordLength = len(wordList)
 
+            # loads whitelisted mod words (for mods like EM:R v EM)
+            with open("modwhitelist.txt") as wordFile:
+                modWhitelist = [wordNames.strip().lower() for wordNames in wordFile.readlines()]
+                wblLength = len(modWhitelist)
+
             # checks if message contains any words in the txt files
             for x in range(mblLength):
                 for y in range(wordLength):
-                    wflRegexA = r"("+wordList[y]+" "+modBlacklist[x]+")"
-                    wflMatchesA = re.finditer(wflRegexA, message.content.lower(), re.MULTILINE | re.IGNORECASE)
-                    for z in enumerate(wflMatchesA, start=1):
-                        # checks if user is not a mod
-                        mod = message.author.guild_permissions.kick_members
-                        if not mod:
-                            # NatBot will warn for now about the link
-                            await message.channel.send(f"{message.author.mention}, you mentioned obtaining a blacklisted mod or download link to one.\nPlease refrain from posting links to those in the near future.")
+                    for z in range(wblLength):
+                        wflRegexA = r"("+wordList[y]+" "+modBlacklist[x]+"$|"+wordList[y]+" "+modBlacklist[x]+" (?!"+modWhitelist[z]+")|)"
+                        wflMatchesA = re.finditer(wflRegexA, message.content.lower(), re.MULTILINE | re.IGNORECASE)
+                        for a in enumerate(wflMatchesA, start=1):
+                            # checks if user is not a mod
+                            mod = message.author.guild_permissions.kick_members
+                            if not mod:
+                                # NatBot will warn for now about the link
+                                await message.channel.send(f"{message.author.mention}, you mentioned obtaining a blacklisted mod or download link to one.\nPlease refrain from posting links to those in the near future.")
 
 def setup(bot):
     bot.add_cog(BlacklistCog(bot))
